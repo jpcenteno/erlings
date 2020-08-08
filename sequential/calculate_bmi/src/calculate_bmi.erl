cba23a027e6b33441524d51224a940f2adb93a2e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-08 20:37:01 -0300

    Solve sequential/calculate_bmi

--- sequential/calculate_bmi/src/calculate_bmi.erl
+++ sequential/calculate_bmi/src/calculate_bmi.erl
@@ -4,8 +4,13 @@
 
 -include("../src/person_record.hrl").
 
-bmi(Person) ->
-  put_your_solution_here.
+bmi(#person{weight=Weight, height=Height}) ->
+  Weight / (Height * Height).
 
 classify(Person) ->
-  put_your_solution_here.
+  case bmi(Person) of
+    BMI when BMI < 18.5                      -> underweight;
+    BMI when (18.5 =< BMI) and (BMI < 25.0)  -> normal;
+    BMI when (25.0 =< BMI) and (BMI =< 30.0) -> overweight;
+    _                                        -> obese
+  end.
