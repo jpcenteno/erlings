708834c2378930c21fd7859f2d13f04211ade2d4
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-08 20:16:25 -0300

    Solve sequential/bank_accounts

--- sequential/bank_accounts/src/bank_account.erl
+++ sequential/bank_accounts/src/bank_account.erl
@@ -2,5 +2,24 @@
 
 -export([process_operation/2]).
 
-process_operation(Bank, Operation) ->
-  put_your_solution_here.
+process_operation(Bank, {AccountNumber, withdraw, Amount}) ->
+  Account = lists:keyfind(AccountNumber, 1, Bank),
+  case Account of
+    false -> {error, account_not_found};
+    _     -> withdraw(Account, Amount)
+  end;
+
+process_operation(Bank, {AccountNumber, deposit, Amount}) ->
+  Account = lists:keyfind(AccountNumber, 1, Bank),
+  case Account of
+    false -> {error, account_not_found};
+    _     -> deposit(Account, Amount)
+  end.
+
+withdraw({AccountNumber, AccountAmount}, WithdrawalAmount) when WithdrawalAmount =< AccountAmount ->
+  {AccountNumber, AccountAmount - WithdrawalAmount};
+withdraw(_Account, _WithdrawalAmount) ->
+  {error, insufficient_funds}.
+
+deposit({AccountNumber, AccountAmount}, WithdrawalAmount) ->
+  {AccountNumber, AccountAmount + WithdrawalAmount}.
