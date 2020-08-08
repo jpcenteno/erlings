a80f797a1200eaa39541aa0cd6f097beee3451ec
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-08 20:51:11 -0300

    solve sequential/insert_element_at

--- sequential/insert_element_at/src/insert_element_at.erl
+++ sequential/insert_element_at/src/insert_element_at.erl
@@ -2,5 +2,16 @@
 
 -export([insert/3]).
 
-insert(_List, _Pos, _Element) ->
-  believe_in_yourself.
+insert(List, Pos, Element) ->
+  Fun = fun (OldValue) -> update_velue(OldValue, Element) end,
+  update_nth(Pos, List, Fun).
+
+update_nth(1, [H | T], Fun) ->
+  [Fun(H) | T];
+update_nth(Nth, [H | T], Fun) ->
+  [H | update_nth(Nth - 1, T, Fun)].
+
+update_velue(#{current := OldValue}, NewValue) ->
+  #{current => NewValue, old => OldValue};
+update_velue(OldValue, NewValue) ->
+  #{current => NewValue, old => OldValue}.
