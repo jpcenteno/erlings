fed2ad0f527ff40f9b47fe881243bc95ac33dc6f
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-08 21:04:20 -0300

    Solve filter_fibonacci_numbers

--- sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
+++ sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
@@ -1,6 +1,17 @@
 -module(filter_fibonacci_numbers).
 
--export([filter/1]).
+-export([filter/1, is_fib/1]).
 
 filter(List) ->
-  put_your_solution_here.
+  [X || X <- List, is_fib(X)].
+
+is_fib(1) -> true;
+is_fib(X) when X >= 2 ->
+  is_fib(X, 2, 1).
+
+is_fib(X, Fi, _Fj) when X < Fi->
+  false;
+is_fib(X, X, _Fj) ->
+  true;
+is_fib(X, Fi, Fj) ->
+  is_fib(X, Fi + Fj, Fi).
--- sequential/filter_fibonacci_numbers/test/filter_fibonacci_numbers_test.erl
+++ sequential/filter_fibonacci_numbers/test/filter_fibonacci_numbers_test.erl
@@ -2,6 +2,13 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+is_fib_test() ->
+  ?assert(filter_fibonacci_numbers:is_fib(1)),
+  ?assert(filter_fibonacci_numbers:is_fib(2)),
+  ?assert(filter_fibonacci_numbers:is_fib(3)),
+  ?assertNot(filter_fibonacci_numbers:is_fib(4)),
+  ?assert(filter_fibonacci_numbers:is_fib(5)).
+
 filter_fibonacci_numbers_test() ->
   List = [1, 2, 3, 4, 5, 7, 8, 9, 10],
   ?assertEqual([1, 2, 3, 5, 8], filter_fibonacci_numbers:filter(List)).
