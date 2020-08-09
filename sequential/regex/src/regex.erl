8ba02df515d83c695ebec347369fda57d209c381
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-08 23:22:32 -0300

    Solve sequential/regex

--- sequential/regex/src/regex.erl
+++ sequential/regex/src/regex.erl
@@ -2,5 +2,29 @@
 
 -export([match/2]).
 
-match(_String, _Regex) ->
-    put_your_solution_here.
+match(_String, "") ->
+    false;
+match(String, Regex) ->
+    match_aux(string:lowercase(String), string:lowercase(Regex)).
+
+match_aux(_String, "") ->
+    true;
+
+match_aux([Literal | StringTail], [Literal, $? | RegexTail]) ->
+    io:format("You are here~n"),
+    match_aux(StringTail, RegexTail);
+
+match_aux(String, [_Literal, $? | RegexTail]) ->
+    match_aux(String, RegexTail);
+
+match_aux([Literal | StringTail], [Literal, $* | _RegexTail] = Regex) ->
+    match_aux(StringTail, Regex);
+
+match_aux(String, [Literal, $* | RegexTail]) ->
+    match_aux(String, RegexTail);
+
+match_aux([_Char | StringTail], [$. | RegexTail]) ->
+    match_aux(StringTail, RegexTail);
+
+match_aux([Char | StringTail], [Literal | RegexTail]) ->
+    (Char =:= Literal) andalso match_aux(StringTail, RegexTail).
--- sequential/regex/test/regex_test.erl
+++ sequential/regex/test/regex_test.erl
@@ -1,10 +1,19 @@
 -module(regex_test).
 -include_lib("eunit/include/eunit.hrl").
 
-matches_characters_test() -> 
+empty_regex_matches_nothing_test() ->
+    ?assertNot(regex:match("foo", "")).
+
+matches_characters_test() ->
     ?assert(regex:match("a", "a")).
 
-matches_numbers_test() -> 
+char_literals_dont_match_other_characters_test() ->
+    ?assertNot(regex:match("b", "a")).
+
+matches_first_number_but_mismatches_the_second_test() ->
+    ?assertNot(regex:match("42", "49")).
+
+matches_numbers_test() ->
     ?assert(regex:match("49", "49")).
 
 matches_question_mark_test() ->
