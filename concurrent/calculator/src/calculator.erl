c42565bd56f7458e7a3cbd9ecbb3b8d9f9b2a6f0
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-09 12:11:23 -0300

    Solve concurrent/calculator

--- concurrent/calculator/src/calculator.erl
+++ concurrent/calculator/src/calculator.erl
@@ -1,13 +1,69 @@
 -module(calculator).
 -export([start_calculator/0,
          calculator_server/0,
-         turn_off/1]).
+         turn_off/1,
+         add/3,
+         subtract/3,
+         multiply/3,
+         divide/3]).
+
+%% -------------------------------------------------------------------------
+%% Calculator API
+%% -------------------------------------------------------------------------
 
 start_calculator() ->
-  put_your_solution_here.
-
-calculator_server() ->
-  put_your_solution_here.
+  spawn(calculator, calculator_server, []).
 
 turn_off(Pid) ->
-  put_your_solution_here.
+  Pid ! off.
+
+add(Cal, X, Y) ->
+  Cal ! {self(), {add, X, Y}},
+  receive
+    {Cal, Result} -> Result
+  end.
+
+subtract(Cal, X, Y) ->
+  Cal ! {self(), {subtract, X, Y}},
+  receive
+    {Cal, Result} -> Result
+  end.
+
+multiply(Cal, X, Y) ->
+  Cal ! {self(), {multiply, X, Y}},
+  receive
+    {Cal, Result} -> Result
+  end.
+
+divide(Cal, X, Y) ->
+  Cal ! {self(), {divide, X, Y}},
+  receive
+    {Cal, Result} -> Result
+  end.
+
+%% -------------------------------------------------------------------------
+%% Calculator internals
+%% -------------------------------------------------------------------------
+
+calculator_server() ->
+  receive
+
+    {Pid, {add, X, Y}} ->
+      Pid ! {self(), X + Y};
+
+    {Pid, {subtract, X, Y}} ->
+      Pid ! {self(), X - Y};
+
+    {Pid, {multiply, X, Y}} ->
+      Pid ! {self(), X * Y};
+
+    {Pid, {divide, X, Y}} ->
+      Pid ! {self(), X / Y};
+
+    off ->
+      io:format("Bye from ~p~n", [self()]);
+
+    UnknownMessage ->
+      io:format("I don't understand message ~w~n", [UnknownMessage]),
+      calculator_server()
+  end.
