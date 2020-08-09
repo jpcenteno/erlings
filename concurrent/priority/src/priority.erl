4161c90abb7bd9e298bedaf34b74b17799df3def
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-09 14:01:40 -0300

    Solve concurrent/priority

--- concurrent/priority/src/priority.erl
+++ concurrent/priority/src/priority.erl
@@ -5,10 +5,25 @@
          priority_loop/1]).
 
 start() ->
-  your_solution_here.
+  spawn(?MODULE, priority_loop, [[]]).
+
 
 get_messages(Pid) ->
-  your_solution_here.
+  Pid ! {get_messages, self()},
+  receive
+    {Pid, List} ->
+      %% I decided to do the reversing on the caller process to prevent the
+      %% `priority_loop` process from blocking.
+      lists:reverse(List)
+  end.
 
-priority_loop(State) ->
-  your_solution_here.
+priority_loop(List) ->
+  receive
+    {vip, _} = Msg -> priority_loop([Msg | List])
+    after 0 ->
+      receive
+        {normal, _} = Msg -> priority_loop([Msg | List]);
+        {get_messages, Pid} -> Pid ! {self(), List}
+        after 0 -> priority_loop(List)
+      end
+    end.
