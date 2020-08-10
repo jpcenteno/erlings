c8ed66755c75c509584a807c41c4d06c796bc378
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-08-10 00:46:44 -0300

    Solve otp/shopping_cart

--- otp/shopping_cart/src/shopping_cart.erl
+++ otp/shopping_cart/src/shopping_cart.erl
@@ -2,20 +2,40 @@
 
 -behaviour(gen_server).
 
--export([start_link/0,
-         put_item/2,
-         finish/1,
-         cost_so_far/1,
-        ]).
+-export([start_link/0, put_item/2, finish/1, cost_so_far/1]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 start_link() ->
-     put_your_solution_here.
+    gen_server:start_link(?MODULE, [], []).
 
 put_item(Pid, Item) ->
-    put_your_solution_here.
+    gen_server:call(Pid, {put_item, Item}).
 
 cost_so_far(Pid) ->
-    put_your_solution_here.
+    gen_server:call(Pid, {cost_so_far}).
 
 finish(Pid) ->
-    put_your_solution_here.
+    gen_server:call(Pid, {finish}).
+
+%% -----------------------------------------------------------------------------
+%% Private
+%% -----------------------------------------------------------------------------
+
+init(Items) ->
+    {ok, Items}.
+
+handle_call({put_item, Item}, _From, Items) ->
+    NewItems = [Item | Items],
+    {reply, NewItems, NewItems};
+handle_call({cost_so_far}, _From, Items) ->
+    {reply, total_cost(Items), Items};
+handle_call(finish, _From, Items) ->
+    {stop, normal, ok, total_cost(Items)}.
+
+
+total_cost(Items) ->
+    lists:sum([Cost || {_Name, Cost} <- Items]).
+
+%%% Generic handle_cast. Unused here.
+handle_cast(_Request, State) ->
+  {noreply, State}.
